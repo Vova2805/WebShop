a6ff15fb175a99e671e9a6ba78d44722286578b6
Volodymyr Dudas <vovadudas@gmail.com>
2016-08-03 06:01:49 +0300

    dump

---
+++ src/main/resources/database/dump/webshopdb_propertyClass.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for osx10.9 (x86_64)
+--
+-- Host: localhost    Database: webshopdb
+-- ------------------------------------------------------
+-- Server version	5.7.13
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `propertyClass`
+--
+
+LOCK TABLES `propertyClass` WRITE;
+/*!40000 ALTER TABLE `propertyClass` DISABLE KEYS */;
+INSERT INTO `propertyClass` VALUES (1,'Installation method',''),(3,'Refrigerator Type',''),(4,' The total volume of the refrigerator',''),(5,'Display',''),(6,'Noise level',''),(13,'Dimensions (HxWxD)',''),(14,'Colour',''),(16,'CPU',''),(17,'The amount of RAM',''),(18,'Type of memory',''),(19,'Operating system',''),(20,' The weight',''),(21,'HDD',''),(22,'SSD',''),(23,'Manufacturer','');
+/*!40000 ALTER TABLE `propertyClass` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-08-03  5:58:54
